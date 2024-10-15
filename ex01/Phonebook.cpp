9401b14abdaea3989e9d49412123059eab34feaf
tamasandor <tamas.andor@yahoo.com>
2024-10-15 10:39:58 +0200

    Fix segfault after the 8th contact

--- ex01/Phonebook.cpp
+++ ex01/Phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 12:42:33 by atamas            #+#    #+#             */
-/*   Updated: 2024/10/14 19:10:00 by atamas           ###   ########.fr       */
+/*   Updated: 2024/10/15 10:39:07 by atamas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ void	add_to_contact_array(PhoneBook &book, Contact &new_contact)
 	Contact	temp;
 	int		i;
 
-	if (book.size < 8)
+	if (book.size < 7)
 		i = book.size;
 	else
 		i = 7;
@@ -82,27 +82,26 @@ void	create_contact(PhoneBook &book)
 
 	std::cout << "First name: ";
 	std::getline(std::cin, firstname);
-	filled = true;
-	// std::cout << "Last name: ";
-	// std::getline(std::cin, lastname);
-	// std::cout << "Nickname: ";
-	// std::getline(std::cin, nickname);
-	// std::cout << "Phone number: ";
-	// std::getline(std::cin, phone_number);
-	// std::cout << "Darkest secret: ";
-	// std::getline(std::cin, darkest_secret);
+	std::cout << "Last name: ";
+	std::getline(std::cin, lastname);
+	std::cout << "Nickname: ";
+	std::getline(std::cin, nickname);
+	std::cout << "Phone number: ";
+	std::getline(std::cin, phone_number);
+	std::cout << "Darkest secret: ";
+	std::getline(std::cin, darkest_secret);
 
-	// if ((firstname.empty() == 0 || !isOnlyWhitespace(firstname)) && (lastname.empty() == 0 || !isOnlyWhitespace(lastname))
-	// 	&& (nickname.empty() == 0 || !isOnlyWhitespace(nickname)) && (phone_number.empty() == 0 || !isOnlyWhitespace(phone_number))
-	// 		 && isOnlyNumbers(phone_number) && (darkest_secret.empty() == 0 || !isOnlyWhitespace(darkest_secret)))
-	// 			filled = true;
+	if ((firstname.empty() == 0 || !isOnlyWhitespace(firstname)) && (lastname.empty() == 0 || !isOnlyWhitespace(lastname))
+		&& (nickname.empty() == 0 || !isOnlyWhitespace(nickname)) && (phone_number.empty() == 0 || !isOnlyWhitespace(phone_number))
+			 && isOnlyNumbers(phone_number) && (darkest_secret.empty() == 0 || !isOnlyWhitespace(darkest_secret)))
+				filled = true;
 	if (filled == true)
 	{
 		tmp.setFirstName(firstname);
-		// tmp.setLastName(lastname);
-		// tmp.setNickname(nickname);
-		// tmp.setPhoneNumber(phone_number);
-		// tmp.setDarkestSecret(darkest_secret);
+		tmp.setLastName(lastname);
+		tmp.setNickname(nickname);
+		tmp.setPhoneNumber(phone_number);
+		tmp.setDarkestSecret(darkest_secret);
 		tmp.index = 1;
 		add_to_contact_array(book, tmp);
 	}
--- ex01/Phonebook.hpp
+++ ex01/Phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 13:53:09 by atamas            #+#    #+#             */
-/*   Updated: 2024/10/11 17:32:16 by atamas           ###   ########.fr       */
+/*   Updated: 2024/10/15 10:34:54 by atamas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ class PhoneBook
 	public:
 		PhoneBook(void);
 		~PhoneBook();
-		Contact	contacts[7];
+		Contact	contacts[8];
 		int		size;
 };
 
