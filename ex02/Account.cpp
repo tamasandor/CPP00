acb62fcc2e8c3d42b6b7fe49013f7ac1143b89bc
atamas <tamas.andor@yahoo.com>
2024-10-16 17:00:01 +0200

    Implement the file by reverse engineering the header file and analyzing the logs; the timestamp needs to be implemented.

---
+++ ex02/Account.cpp
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 10:54:25 by atamas            #+#    #+#             */
+/*   Updated: 2024/10/16 16:51:10 by atamas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int	initial_deposit)
+{
+	_accountIndex = _nbAccounts;
+	_nbAccounts += 1;
+	_amount = initial_deposit;
+	_totalAmount += initial_deposit;
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << '\n';
+}
+
+Account::~Account()
+{
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << '\n';
+}
+
+int	Account::getNbAccounts( void )
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount( void )
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits( void )
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return (_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos( void )
+{
+	std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << '\n';
+	return ;
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	int	pre_amount;
+
+	pre_amount = _amount;
+	_totalNbDeposits += 1;
+	_totalAmount += deposit;
+	_amount += deposit;
+	_nbDeposits += 1;
+	std::cout << "index:" << _accountIndex << ";p_amount:" << pre_amount << ";deposit:" << deposit << ";amount:" << _amount << ";nb_deposits:" << _nbDeposits << "\n";
+	return ;
+}
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	int	pre_amount;
+
+	pre_amount = _amount;
+	if (_amount - withdrawal >= 0)
+	{
+		_nbWithdrawals += 1;
+		_amount -= withdrawal;
+		_totalAmount -= withdrawal;
+		_totalNbWithdrawals += 1;
+		std::cout << "index:" << _accountIndex << ";p_amount:" << pre_amount << ";withdrawal:" << withdrawal << ";amount:" << _amount << ";nb_withdrawals:" << _nbWithdrawals << '\n';
+		
+		return (true);
+	}
+	std::cout << "index:" << _accountIndex << ";p_amount:" << pre_amount << ";withdrawal:refused\n";
+	return (false);
+}
+int		Account::checkAmount( void ) const
+{
+	return (0);
+}
+void	Account::displayStatus( void ) const
+{
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << '\n';
+	return ;
+}
+
+void	displayTimestamp( void )
+{
+	return ;
+}
